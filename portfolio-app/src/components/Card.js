a52b60d9bdf640eb3a15c258079fba906e27833a
Seth Nowac <98350860+SethNowac@users.noreply.github.com>
2023-11-17 15:48:04 -0500

    Continued working on projects page

--- portfolio-app/src/components/Card.js
+++ portfolio-app/src/components/Card.js
@@ -1,4 +1,5 @@
 import "./Card.css";
+import { useState } from "react";
 
 /**
  * A component that represents a card containing an image and child elements.
@@ -7,9 +8,44 @@ import "./Card.css";
  * @param {string} props.image - The URL of the image to display on the card.
  * @returns {JSX.Element} - A JSX element representing the card.
  */
-function Card({ children, image }) {
+function Card({ children, image, callback }) {
+  const [mouseOverButton, setMouseOverbutton] = useState(false);
+
+  const cardStyle = {
+    textAlign: "center",
+    boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+    borderRadius: "5px",
+    maxWidth: "300px",
+    maxHeight: "300px",
+    backgroundColor:"rgb(131, 131, 131)",
+    padding: "5px 5px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    fontSize: "calc(10px)"
+};
+
+const activeCardStyle = {
+  textAlign: "center",
+  boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+  borderRadius: "5px",
+  maxWidth: "310px",
+  maxHeight: "310px",
+  backgroundColor:"rgb(161, 161, 161)",
+  padding: "5px 5px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  fontSize: "calc(10px)"
+};
+
   return (
-    <div className="card">
+    <div 
+      style={mouseOverButton ? activeCardStyle : cardStyle}
+      onMouseEnter={() => setMouseOverbutton(true)}
+      onMouseLeave={() => setMouseOverbutton(false)}
+      onClick={()=>{callback()}}
+    >
       {image && <img className="card-img" src={image} alt=""></img>}
       {children}
     </div>
--- portfolio-app/src/pages/Projects.js
+++ portfolio-app/src/pages/Projects.js
@@ -1,9 +1,17 @@
 import Card from "../components/Card";
+import CarDodgeImage from "../images/car_dodge_screenshot.png";
 
 export default function Projects() {
     return (
-        <div>
-            <Card>
+        <div style={{
+            margin:"40px"
+        }}>
+            <h1 style={{
+                color: "#dddfea"
+            }}>Games</h1>
+            <Card image={CarDodgeImage} callback={()=> {
+
+            }}>
                 <h1>Car Dodge</h1>
             </Card>
         </div>
